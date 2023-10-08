71e119428a37690c17c3857e89164da59b9caa8d
E9ex <egecansar@gmail.com>
2023-10-08 13:08:12 +0300

    updatedbloodscreenwhenheath decrease

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -7,15 +7,28 @@ using Random = UnityEngine.Random;
 
 public class PlayerHealth : MonoBehaviour
 {
+  
     private float lerpTimer;
     private float health;
+    [Header("Health")]
     public float maxhealth = 100f;
     public float chipSpeed = 2f;
     public Image fronthealthbar;
     public Image backhealthBar;
+
+    [Header("DamageOverlay")] 
+    public Image overlay;
+    public float duration;
+    public float fadespeed;
+    private float durationTimer;
+    
+    
+    
+    
     void Start()
     {
         health = maxhealth;
+        overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b,0);
     }
 
     // Update is called once per frame
@@ -23,6 +36,18 @@ public class PlayerHealth : MonoBehaviour
     {
         health = Mathf.Clamp(health, 0, maxhealth);
         updatehealthUI();
+        if (overlay.color.a>0)
+        {
+            if (health<30)
+                return;
+            durationTimer += Time.deltaTime;
+            if (durationTimer>duration)
+            {
+                float tempAlpha = overlay.color.a;
+                tempAlpha -= Time.deltaTime * fadespeed;
+                overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b,tempAlpha);
+            }
+        }
     }
 
     public void updatehealthUI()
@@ -56,7 +81,8 @@ public class PlayerHealth : MonoBehaviour
     {
         health -= damage;
         lerpTimer = 0f;
-
+        durationTimer = 0;
+        overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b,1);
     }
 
     public void restoreHealth(float healAmount)
